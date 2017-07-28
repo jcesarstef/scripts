f2d327cb843ad1dacf9471d379c4f71d8efd20e7
Julio C. Stefanutto <jcesarstef@gmail.com>
2017-07-27 21:08:11 -0300

    Update simple_http_brute_forcer.sh

--- simple_http_brute_forcer.sh
+++ simple_http_brute_forcer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Use: sh simple_http_brute_forcer.sh "domain" "wordlist.txt"
-for i in $(cat $2); do echo -n "$i "; curl -sL -I $1/$i | head -1 ; done
+for i in $(cat $2); do echo -n "$i "; curl -s -I $1/$i | head -1 ; done
